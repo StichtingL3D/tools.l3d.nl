891e600aba404bfd5e9b2a00242da1ee592eb546
Lode Claassen <lode@alsvanzelf.nl>
2012-05-06 21:08:12 +0200

    added objects table

--- backend/models/sql/objects.sql
+++ backend/models/sql/objects.sql
@@ -2,9 +2,10 @@
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `objects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` enum('avatars','models','seqs','sounds','textures') DEFAULT NULL,
-  `filename` varchar(75) DEFAULT NULL,
-  `objectpath_id` smallint(6) DEFAULT NULL,
+  `type` enum('avatars','models','seqs','sounds','textures') NOT NULL,
+  `filename` varchar(75) NOT NULL,
+  `upload_time` int(11) DEFAULT NULL,
+  `objectpath_id` smallint(6) NOT NULL,
   `citizen_id` smallint(6) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
